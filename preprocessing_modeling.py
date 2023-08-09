750af3a2e1c78750173da06207c8ec2ffafe1947
riosniper87 <60447415+riosniper87@users.noreply.github.com>
2023-08-09 11:57:18 +0900

    add concat_lst function

--- preprocessing_modeling.py
+++ preprocessing_modeling.py
@@ -18,21 +18,22 @@ class Preprocess:
     default=None,
     metadata={"help": "A list of names of character columns"},
     )
-    Dummy : List[List[int]] = field(
+    Dummy : List[List[str]] = field(
         default=None,
         metadata={"help": "A list of names of categorical columns to treat as dummy variable"},
     )        
-    Embedding : List[List[int]] = field(
+    Embedding : List[List[str]] = field(
     default=None,
     metadata={"help": "A list of names of dummy columns to treat as embeddeding variable"},
     )        
 
 logger = logging.getLogger(__name__)        
 
-def embedding(lst, data): 
+def embedding(form, data): 
     dict_ls = []
     embedding_df = pd.DataFrame()
-    for embedding_list in lst:
+    
+    for embedding_list in form.Embedding:
         merged_list = pd.concat([data[col] for col in embedding_list], ignore_index=True)
         unique_lst = list(set(merged_list))
         label_encoder = LabelEncoder()
@@ -46,18 +47,39 @@ def embedding(lst, data):
     
     return embedding_df, dict_ls
 
+# def embedding(lst, data): 
+#     dict_ls = []
+#     embedding_df = pd.DataFrame()
+    
+#     for embedding_list in lst:
+#         merged_list = pd.concat([data[col] for col in embedding_list], ignore_index=True)
+#         unique_lst = list(set(merged_list))
+#         label_encoder = LabelEncoder()
+#         encode = label_encoder.fit_transform(unique_lst)
+#         encode_dict = {element: encoded_value for element, encoded_value in zip(unique_lst, encode)}
+#         dict_ls.append(encode_dict)
+#         lst_df = pd.DataFrame()
+#         for i in embedding_list: 
+#             lst_df[f'{i}'] = data[f'{i}'].map(encode_dict)
+#         embedding_df = pd.concat([embedding_df,lst_df], axis=1)
+    
+#     return embedding_df, dict_ls
+
 def dummy(lst, data): 
 #     dict_ls = []
     dummy_df = pd.DataFrame()
+    
     for dummy_list in lst:
         for i in lst:
             dummy_cols = pd.get_dummies(data[i], prefix=i)
             dummy_df = pd.concat([dummy_df, dummy_cols], axis=1)
     return dummy_df
 
-print("you working?")
-print("you working?")
-print("for sure?")
-print("study session")
-# def preprocess(data, Continuous, Character, Embedding, Dummy):
+def concat_lst(lstlst):
+    concat_lst = []
+    for lst in lstlst: 
+        concat_lst.extend(lst)
+    return concat_lst
+
+# def preprocess(data, Embedding, Dummy):
 # let's push by function 
